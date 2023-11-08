8a952a7c98cae9d10e90fbdbfcbc6d3369295b93
Petr (Laptop) <petrbohac3@seznam.cz>
2023-11-08 20:14:19 +0100

    mqtt form

--- frontend/src/components/configuration-section/ConnectivitySection.vue
+++ frontend/src/components/configuration-section/ConnectivitySection.vue
@@ -6,7 +6,8 @@ const subsections = [
   {
     id: 'mqtt',
     label: 'MQTT',
-    component: MQTTSubsection
+    component: MQTTSubsection,
+    open: true
   }
 ];
 
@@ -16,15 +17,18 @@ const subsections = [
   <div class="accordion" id="connectivityAccordion">
     <div class="accordion-item" v-for="subsection in subsections" :key="subsection.id">
       <h2 class="accordion-header">
-        <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse"
+        <button class="accordion-button" :class="!subsection.open?'collapsed':''" type="button"
+                data-bs-toggle="collapse"
                 :data-bs-target="`#${subsection.id}-collapse`"
                 aria-expanded="false" :aria-controls="`#${subsection.id}-collapse`">
           {{ subsection.label }}
         </button>
       </h2>
-      <div :id="`${subsection.id}-collapse`" class="accordion-collapse collapse"
+      <div :id="`${subsection.id}-collapse`" class="accordion-collapse collapse" :class="subsection.open?'show':''"
            data-bs-parent="#connectivityAccordion">
-        <Component :is="subsection.component"/>
+        <div class="accordion-body">
+          <Component :is="subsection.component"/>
+        </div>
       </div>
     </div>
   </div>
--- frontend/src/components/configuration-section/subsection/MQTTSubsection.vue
+++ frontend/src/components/configuration-section/subsection/MQTTSubsection.vue
@@ -1,9 +1,32 @@
 <script setup>
 
+const DEFAULT_MQTT_PORT = 1883;
+
 </script>
 
 <template>
-  mqtt shits
+  <form>
+    <div class="row mb-3">
+      <div class="col">
+        <input type="text" class="form-control" placeholder="Username">
+      </div>
+      <div class="col">
+        <input type="password" class="form-control" placeholder="Password">
+      </div>
+    </div>
+    <div class="row mb-3">
+      <div class="col">
+        <div class="input-group">
+          <span class="input-group-text">Broker settings</span>
+          <input type="text" class="form-control" placeholder="Server address">
+          <input type="number" class="form-control" :placeholder="DEFAULT_MQTT_PORT">
+        </div>
+      </div>
+    </div>
+    <div class="text-end">
+      <button type="submit" class="btn btn-primary text-end">Save changes</button>
+    </div>
+  </form>
 </template>
 
 <style scoped>
