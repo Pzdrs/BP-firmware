6536f040dacffad1885f84ec4abb9a36fa34f5e3
Petr (Desktop) <petrbohac3@seznam.cz>
2023-11-19 17:45:10 +0100

    mqtt form fully funcional

--- frontend/src/components/configuration-section/subsection/connectivity/MQTTSubsection.vue
+++ frontend/src/components/configuration-section/subsection/connectivity/MQTTSubsection.vue
@@ -1,9 +1,9 @@
 <script setup>
 
 import {mqttConfigEndpoint, mqttTestEndpoint} from "../../../../axios/endpoints";
-import {ref} from "vue";
+import {onMounted, ref} from "vue";
 
-const DEFAULT_MQTT_PORT = 1883;
+const DEFAULT_MQTT_PORT = '1883';
 
 const testing = ref(false);
 const dryRun = ref(false);
@@ -21,14 +21,13 @@ function handleSubmit(event) {
   saving.value = true;
   alerts.value = [];
 
-  const username = event.target.username.value;
-  const password = event.target.password.value;
-  const server = event.target.serverAddress.value;
-  const port = event.target.port.value || DEFAULT_MQTT_PORT;
+  const data = new FormData();
+  data.append("username", event.target.username.value);
+  data.append("password", event.target.password.value);
+  data.append("server", event.target.serverAddress.value);
+  data.append("port", event.target.port.value || DEFAULT_MQTT_PORT);
 
-  mqttConfigEndpoint.post({
-    username, password, server, port
-  }).then(() => {
+  mqttConfigEndpoint.post(data).then(() => {
     alerts.value = [{
       message: "The configuration was saved successfully.",
       class: "alert-success"
@@ -63,13 +62,32 @@ function testConnection() {
         }
       });
 }
+
+function clear() {
+  mqttConfigEndpoint.delete()
+      .then(() => {
+        document.querySelector('#mqtt-form').reset();
+        alerts.value = [{
+          message: "The configuration was cleared successfully.",
+          class: "alert-success"
+        }];
+      });
+}
+
+mqttConfigEndpoint.get().then(response => {
+  const data = response.data;
+  document.querySelector('input[name="username"]').value = data.username;
+  document.querySelector('input[name="password"]').value = data.password;
+  document.querySelector('input[name="serverAddress"]').value = data.server;
+  document.querySelector('input[name="port"]').value = data.port;
+});
 </script>
 
 <template>
   <div v-for="(alert, index) in alerts" :key="index" class="alert" :class="alert.class" role="alert">
     {{ alert.message }}
   </div>
-  <form @submit.prevent="handleSubmit">
+  <form id="mqtt-form" @submit.prevent="handleSubmit">
     <div class="row mb-3">
       <div class="col">
         <input type="text" name="username" class="form-control" placeholder="Username">
@@ -95,6 +113,7 @@ function testConnection() {
         </span>
         Test
       </button>
+      <button class="btn btn-danger me-2" @click.prevent="clear">Clear</button>
       <button type="submit" class="btn btn-primary">Save changes</button>
     </div>
   </form>
